7c212f47cbee15dfaa033a5c95c4cc1d00ad68de
sohyu-na <solim73@naver.com>
2025-07-01 17:08:24 +0900

    docs: initial spring boot project setup

---
+++ src/main/java/efub/cpbr/crumble/CrumbleApplication.java
@@ -0,0 +1,13 @@
+package efub.cpbr.crumble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrumbleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrumbleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/efub/cpbr/crumble/CrumbleApplicationTests.java
@@ -0,0 +1,13 @@
+package efub.cpbr.crumble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrumbleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
