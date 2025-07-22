08bab3bc7ad95d3269abdbcb4422211ee1e8e80c
Hanrann6 <yookrann@ewhain.net>
2025-07-22 16:19:20 +0900

    [Feat] 캘린더, 포춘쿠키 api에 Swagger 설정 추가(#14, #16)

--- src/main/java/efub/cpbr/crumble/calendar/controller/CalendarController.java
+++ src/main/java/efub/cpbr/crumble/calendar/controller/CalendarController.java
@@ -4,6 +4,10 @@ import efub.cpbr.crumble.calendar.dto.AnswerDto;
 import efub.cpbr.crumble.calendar.dto.AnsweredDatesResponse;
 import efub.cpbr.crumble.calendar.service.CalendarService;
 import efub.cpbr.crumble.user.entity.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,27 +24,55 @@ public class CalendarController {
 
     private final CalendarService calendarService;
 
-    // 캘린더에서 월별로 답변한 날짜를 조회
+    // 캘린더에서 월별로 답변한 날짜 조회 컨트롤러
+    @Operation(
+            summary = "포춘 쿠키 열기",
+            description = "하루에 1번 과거의 랜덤 답변을 조회합니다.."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "401", description = "인증 정보 없음")
+    })
     @GetMapping("/calendar/answers")
     public ResponseEntity<AnsweredDatesResponse> getAnsweredDates(//@AuthenticationPrincipal
                                                                   User user,
-                                                                  @RequestParam int year,
-                                                                  @RequestParam int month){
+                                                                  @Parameter(description = "조회 연도", example = "2025")
+                                                                      @RequestParam int year,
+                                                                  @Parameter(description = "조회 월", example = "3")
+                                                                      @RequestParam int month){
         return ResponseEntity.ok(calendarService.getAnsweredDates(user, year, month));
     }
 
     // 월별 답변 목록을 조회
+    @Operation(
+            summary = "포춘 쿠키 열기",
+            description = "하루에 1번 과거의 랜덤 답변을 조회합니다.."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "401", description = "인증 정보 없음")
+    })
     @GetMapping("/answers/me")
     public ResponseEntity<List<AnswerDto>> getMonthlyAnswers(//@AuthenticationPrincipal
                                                              User user,
-                                                             @RequestParam int year,
-                                                             @RequestParam int month
+                                                             @Parameter(description = "조회 연도", example = "2025")
+                                                                 @RequestParam int year,
+                                                             @Parameter(description = "조회 월", example = "3")
+                                                                 @RequestParam int month
     ) {
         List<AnswerDto> answerList = calendarService.getMonthlyAnswers(user, year, month);
         return ResponseEntity.ok(answerList);
     }
 
     // 연속 일수 조회
+    @Operation(
+            summary = "포춘 쿠키 열기",
+            description = "하루에 1번 과거의 랜덤 답변을 조회합니다.."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "401", description = "인증 정보 없음")
+    })
     @GetMapping("/answers/streak")
     public ResponseEntity<Integer> getStreak(//@AuthenticationPrincipal
                                              User user) {
@@ -48,12 +80,22 @@ public class CalendarController {
         return ResponseEntity.ok(streak);
     }
 
+    @Operation(
+            summary = "포춘 쿠키 열기",
+            description = "하루에 1번 과거의 랜덤 답변을 조회합니다.."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "401", description = "인증 정보 없음")
+    })
     //쿠키 개수 조회
     @GetMapping("/cookies")
     public ResponseEntity<Integer> getMonthlyCookieCount(//@AuthenticationPrincipal
                                                          User user,
-                                                         @RequestParam int year,
-                                                         @RequestParam int month
+                                                         @Parameter(description = "조회 연도", example = "2025")
+                                                             @RequestParam int year,
+                                                         @Parameter(description = "조회 월", example = "3")
+                                                             @RequestParam int month
     ) {
         int cookieCount = calendarService.getMonthlyCookieCount(user, year, month);
         return ResponseEntity.ok(cookieCount);
--- src/main/java/efub/cpbr/crumble/item/controller/FortuneController.java
+++ src/main/java/efub/cpbr/crumble/item/controller/FortuneController.java
@@ -4,6 +4,9 @@ import efub.cpbr.crumble.item.dto.FortuneAnswerResponse;
 import efub.cpbr.crumble.item.dto.FortuneUseCheckResponse;
 import efub.cpbr.crumble.item.service.FortuneService;
 import efub.cpbr.crumble.user.entity.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,12 +21,32 @@ public class FortuneController {
 
     private final FortuneService fortuneService;
 
+    //포춘쿠키 조회 컨트롤러
+    @Operation(
+            summary = "포춘 쿠키 열기",
+            description = "하루에 1번 과거의 랜덤 답변을 조회합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "401", description = "인증 정보 없음"),
+            @ApiResponse(responseCode = "403", description = "이미 포춘쿠키 사용함"),
+            @ApiResponse(responseCode = "404", description = "과거의 답변 존재하지 않음")
+    })
     @GetMapping
     public ResponseEntity<FortuneAnswerResponse> getFortune(//@AuthenticationPrincipal
                                                             User user) {
         return ResponseEntity.ok(fortuneService.getFortune(user));
     }
 
+    //포춘쿠키 존재 여부 확인 컨트롤러
+    @Operation(
+            summary = "포춘 쿠키 여부 확인",
+            description = "오늘의 포춘쿠키가 남아있는지 확인합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "401", description = "인증 정보 없음")
+    })
     @GetMapping("/used")
     public ResponseEntity<FortuneUseCheckResponse> checkTodayUse(//@AuthenticationPrincipal
                                                                  User user) {
