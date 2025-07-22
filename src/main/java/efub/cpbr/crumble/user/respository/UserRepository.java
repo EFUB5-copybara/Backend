d50c6a7a3e7468db24d34bdf2f824b3b5f44f3a5
cotton <72211340+kimeunsom@users.noreply.github.com>
2025-07-22 14:45:06 +0900

    feat: 유저 레포지토리 생성

---
+++ src/main/java/efub/cpbr/crumble/user/respository/UserRepository.java
@@ -0,0 +1,13 @@
+package efub.cpbr.crumble.user.respository;
+
+import efub.cpbr.crumble.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    boolean existsByUsername(String username); // 닉네임 중복 방지
+    boolean existsByEmail(String email); // 이메일 중복 방지
+}
