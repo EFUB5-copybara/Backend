07dbf0728c158088cd63522aa74428f93b746d41
oooooming <minjio030107@ewhain.net>
2025-08-01 22:38:41 +0900

    refactoring: swagger 문서화 코드 추가 및 userdetail 코드 수정

--- src/main/java/efub/cpbr/crumble/community/bookmark/controller/BookmarkController.java
+++ src/main/java/efub/cpbr/crumble/community/bookmark/controller/BookmarkController.java
@@ -1,16 +1,22 @@
 package efub.cpbr.crumble.community.bookmark.controller;
 
+import efub.cpbr.crumble.auth.service.CustomUserDetails;
 import efub.cpbr.crumble.community.bookmark.dto.BookmarkResponseDto;
 import efub.cpbr.crumble.community.bookmark.service.BookmarkService;
 import efub.cpbr.crumble.community.like.dto.response.LikeResponseDto;
 import efub.cpbr.crumble.community.like.service.LikeService;
 import efub.cpbr.crumble.user.entity.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Bookmark", description = "북마크 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/community")
@@ -19,18 +25,38 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     // 북마크 생성
+    @Operation(
+            summary = "북마크 생성",
+            description = "특정 게시글에 대한 북마크를 생성합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "생성 성공"),
+            @ApiResponse(responseCode = "400", description = "요청 데이터 오류"),
+            @ApiResponse(responseCode = "404", description = "게시글 또는 유저 없음")
+    })
     @PostMapping("/{postId}/bookmarks")
     public ResponseEntity<BookmarkResponseDto> createBookmark(@PathVariable Long postId,
-                                                              @AuthenticationPrincipal User user) {
+                                                              @AuthenticationPrincipal CustomUserDetails userDetails) {
+        User user = userDetails.getUser();
         Long userId = user.getUserId();
         BookmarkResponseDto responseDto = bookmarkService.createBookmark(postId, userId);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
     }
 
     // 북마크 삭제
+    @Operation(
+            summary = "북마크 삭제",
+            description = "특정 게시글에 대한 북마크를 삭제합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "삭제 성공"),
+            @ApiResponse(responseCode = "400", description = "요청 데이터 오류"),
+            @ApiResponse(responseCode = "404", description = "북마크 또는 유저 없음")
+    })
     @DeleteMapping("/bookmarks/{bookmarkId}")
     public ResponseEntity<String> deleteBookmark(@PathVariable Long bookmarkId,
-                                                 @AuthenticationPrincipal User user) {
+                                                 @AuthenticationPrincipal CustomUserDetails userDetails) {
+        User user = userDetails.getUser();
         Long userId = user.getUserId();
         bookmarkService.deleteBookmark(bookmarkId, userId);
         return ResponseEntity.ok("북마크가 취소되었습니다.");
--- src/main/java/efub/cpbr/crumble/shop/font/controller/FontController.java
+++ src/main/java/efub/cpbr/crumble/shop/font/controller/FontController.java
@@ -1,8 +1,13 @@
 package efub.cpbr.crumble.shop.font.controller;
 
+import efub.cpbr.crumble.auth.service.CustomUserDetails;
 import efub.cpbr.crumble.shop.font.dto.response.FontResponseDto;
 import efub.cpbr.crumble.shop.font.service.FontService;
 import efub.cpbr.crumble.user.entity.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -10,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Font", description = "폰트 관련 API")
 @RestController
 @RequestMapping("/shops/fonts")
 @RequiredArgsConstructor
@@ -17,22 +23,50 @@ public class FontController {
     private final FontService fontService;
 
     // 폰트 리스트 조회
+    @Operation(
+            summary = "폰트 리스트 조회",
+            description = "전체 폰트 리스트를 조회합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공")
+    })
     @GetMapping
-    public ResponseEntity<List<FontResponseDto>> getFonts(@AuthenticationPrincipal User user) {
+    public ResponseEntity<List<FontResponseDto>> getFonts(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        User user = userDetails.getUser();
         return ResponseEntity.ok(fontService.getFonts(user));
     }
 
     // 폰트 상세 조회
+    @Operation(
+            summary = "폰트 상세 조회",
+            description = "특정 폰트 상세 내용을 조회합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "400", description = "요청 데이터 오류"),
+            @ApiResponse(responseCode = "404", description = "폰트 또는 유저 없음")
+    })
     @GetMapping("/{fontId}/details")
-    public ResponseEntity<FontResponseDto> getFontDetail(@AuthenticationPrincipal User user,
+    public ResponseEntity<FontResponseDto> getFontDetail(@AuthenticationPrincipal CustomUserDetails userDetails,
                                          @PathVariable Long fontId) {
+        User user = userDetails.getUser();
         return ResponseEntity.ok(fontService.getFontDetail(user, fontId));
     }
 
     // 폰트 구매
+    @Operation(
+            summary = "폰트 구매",
+            description = "특정 폰트를 구매합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "구매 성공"),
+            @ApiResponse(responseCode = "400", description = "요청 데이터 오류"),
+            @ApiResponse(responseCode = "404", description = "폰트 또는 유저 없음")
+    })
     @PostMapping("/{fontId}/purchasing")
-    public ResponseEntity<String> purchaseFont(@AuthenticationPrincipal User user,
+    public ResponseEntity<String> purchaseFont(@AuthenticationPrincipal CustomUserDetails userDetails,
                              @PathVariable Long fontId) {
+        User user = userDetails.getUser();
         fontService.purchaseFont(user, fontId);
         return ResponseEntity.ok("폰트 구매가 완료되었습니다.");
     }
