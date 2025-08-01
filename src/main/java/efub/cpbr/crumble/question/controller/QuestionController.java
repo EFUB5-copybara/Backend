2807129f51de8529efc38edee783a7a451b293d4
sohyu-na <solim73@naver.com>
2025-08-01 21:48:47 +0900

    feat: QuestionController에 오늘의 질문과 문법검사 여부 반환 API 추가(#34)

--- src/main/java/efub/cpbr/crumble/question/controller/QuestionController.java
+++ src/main/java/efub/cpbr/crumble/question/controller/QuestionController.java
@@ -1,5 +1,7 @@
 package efub.cpbr.crumble.question.controller;
 
+import efub.cpbr.crumble.auth.service.CustomUserDetails;
+import efub.cpbr.crumble.question.dto.res.TodayQuestionResponse;
 import efub.cpbr.crumble.question.dto.res.QuestionResponse;
 import efub.cpbr.crumble.question.service.QuestionService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -9,8 +11,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
@@ -18,6 +22,7 @@ import java.time.LocalDate;
 @Tag(name = "Question", description = "질문 관련 API")
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/questions")
 public class QuestionController {
     private final QuestionService questionService;
 
@@ -29,9 +34,15 @@ public class QuestionController {
             @ApiResponse(responseCode = "200", description = "조회 성공"),
             @ApiResponse(responseCode = "404", description = "질문 없음")
     })
-    @GetMapping("/questions/{date}")
+    @GetMapping("/{date}")
     public ResponseEntity<QuestionResponse> getQuestion(
             @Parameter(description = "조회할 날짜 (yyyy-MM-dd)", example = "2025-07-08") @PathVariable LocalDate date) {
         return ResponseEntity.ok(questionService.getQuestion(date));
+    }
+
+    @GetMapping("/today")
+    public ResponseEntity<TodayQuestionResponse> getTodayQuestion(
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
+        return ResponseEntity.ok(questionService.getTodayQuestion(userDetails.getUserId()));
     }
 }
