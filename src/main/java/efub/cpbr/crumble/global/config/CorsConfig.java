a6b8995fa61239b6d7a8c6580f665c01d5d9c448
Hanrann6 <127124711+Hanrann6@users.noreply.github.com>
2025-08-05 18:16:44 +0900

    [Feat] log용 Sl4j 어노테이션 추가

--- src/main/java/efub/cpbr/crumble/global/config/CorsConfig.java
+++ src/main/java/efub/cpbr/crumble/global/config/CorsConfig.java
@@ -7,9 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
+@Slf4j
 @Configuration
 //@RequiredArgsConstructor
 @EnableConfigurationProperties(CorsProperties.class)
