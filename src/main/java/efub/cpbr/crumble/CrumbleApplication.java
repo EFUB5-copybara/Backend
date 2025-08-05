549a3af872b9baf4bda963771e9ff254cd85a0f4
Hanrann6 <yookrann@ewhain.net>
2025-08-05 17:22:15 +0900

    [Hosfix] application에 enable import

--- src/main/java/efub/cpbr/crumble/CrumbleApplication.java
+++ src/main/java/efub/cpbr/crumble/CrumbleApplication.java
@@ -1,7 +1,9 @@
 package efub.cpbr.crumble;
 
+import efub.cpbr.crumble.global.config.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
